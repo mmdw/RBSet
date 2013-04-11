dba4a984e63273c731f382b9ff1467e0421b4e34
mmdw <mmdw631@gmail.com>
2013-04-11 21:01:21 +0400

    Visual Studio project

---
+++ RBSetTest/unittest1.cpp
@@ -0,0 +1,28 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+#include "..\RBSet\src\RBSet.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace RBSetTest
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestRemove)
+		{
+			RBSet<int> set;
+			set.put(3);
+			set.put(2);
+			set.put(1);
+			set.put(4);
+
+			set.remove(3);
+
+			Assert::AreEqual(set.size(), static_cast<size_t>(3));
+		}
+
+	};
+}
