8ed73cca457df0ace5341a157869d4ff8a85a783
mmdw <mmdw631@gmail.com>
2013-05-17 20:33:04 +0400

    Тесты

---
+++ RBSetTest/Vector2D.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <istream>
+#include <ostream>
+#include <cassert>
+
+struct Vector2D {
+	float a, b;
+
+	Vector2D() : a(0), b(0) {
+
+	}
+
+	Vector2D(float a, float b) :a(a), b(b) {
+
+	}
+
+	bool operator<(const Vector2D& rhs) const {
+		return a < rhs.a ? true : b < rhs.b;
+	}
+
+	bool operator!=(const Vector2D& rhs) const {
+		return a != rhs.a || b != rhs.b;
+	}
+
+	bool operator==(const Vector2D& rhs) const {
+		return a == rhs.a && b == rhs.b;
+	}
+};
+
+std::ostream&  operator<<(std::ostream& os, const Vector2D& v) {
+	os << '(' << v.a << "; " << v.b << ')';
+	return os;
+}
+
+std::istream& operator>>(std::istream& is,  Vector2D& v) {
+	assert(is.get() == '(');
+	is >> v.a;
+
+	assert(is.get() == ';');
+	is >> v.b;
+
+	assert(is.get() == ')');
+
+	return is;
+}
--- RBSetTest/unittest1.cpp
+++ RBSetTest/unittest1.cpp
@@ -1,7 +1,11 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
 
+#include <iostream>
+#include <sstream>
+
 #include "..\RBSet\src\RBSet.h"
+#include "Vector2D.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -11,6 +15,9 @@ namespace RBSetTest
 	{
 	public:
 		
+		/**
+		 * Удаление элемента
+		 */
 		TEST_METHOD(TestRemove)
 		{
 			RBSet<int> set;
@@ -24,6 +31,9 @@ namespace RBSetTest
 			Assert::AreEqual(set.size(), static_cast<size_t>(3));
 		}
 
+		/**
+		 * Копирование
+		 */
 		TEST_METHOD(TestCopy)
 		{
 			RBSet<int> set;
@@ -36,7 +46,52 @@ namespace RBSetTest
 
 			RBSet<int> set1(set);
 
-			//Assert::AreEqual(static_cast<size_t>(3), set1.size());
+			Assert::AreEqual(static_cast<size_t>(3), set1.size());
+		}
+
+		/**
+		 * Vector2D
+		 */
+		TEST_METHOD(TestVector2D)
+		{
+			RBSet<Vector2D> set;
+			set.put(Vector2D(0, 5));
+			set.put(Vector2D(0, 0));
+			set.put(Vector2D(0, 1));
+			set.put(Vector2D(0, 2));			
+
+			Assert::AreEqual(static_cast<size_t>(4), set.size());
+
+			Assert::IsTrue(set.contains(Vector2D(0, 0)));
+			Assert::IsTrue(set.contains(Vector2D(0, 1)));
+			Assert::IsTrue(set.contains(Vector2D(0, 2)));
+			Assert::IsTrue(set.contains(Vector2D(0, 5)));
+
+			Assert::IsFalse(set.contains(Vector2D(99, 99)));
+		}
+
+		/**
+		 * Vector2D Serialize
+		 */
+		TEST_METHOD(TestVector2DSerialization)
+		{
+			RBSet<Vector2D> A;
+			A.put(Vector2D(0, 5));
+			A.put(Vector2D(0, 0));
+			A.put(Vector2D(0, 1));
+			A.put(Vector2D(0, 2));			
+
+			std::ostringstream oss;
+			A.serialize(oss);
+
+			std::istringstream iss(oss.str());
+			RBSet<Vector2D> B(iss);
+
+			Assert::AreEqual(static_cast<size_t>(4), B.size());
+			Assert::IsTrue(B.contains(Vector2D(0, 0)));
+			Assert::IsTrue(B.contains(Vector2D(0, 1)));
+			Assert::IsTrue(B.contains(Vector2D(0, 2)));
+			Assert::IsTrue(B.contains(Vector2D(0, 5)));
 		}
 	};
 }
